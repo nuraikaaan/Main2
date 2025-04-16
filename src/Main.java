6a6cc32dc3f02cd588d3271bc48ff7f5a1e84968
nuraikaaan <alekabaj@gmail.com>
2025-04-16 10:24:01 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,44 @@
-// task 1
-import java.util.Scanner;
-public class Main {
-    public static int findMin(int[] arr) {
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        return min;
-    }
+{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        System.out.println("Testing MyStack:");
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(10);
+        stack.push(20);
+        System.out.println(stack.pop()); // 20
+        System.out.println(stack.peek()); // 10
+
+        System.out.println("\nTesting MyQueue:");
+        MyQueue<String> queue = new MyQueue<>();
+        queue.enqueue("A");
+        queue.enqueue("B");
+        System.out.println(queue.dequeue()); // A
+        System.out.println(queue.peek()); // B
+
+        System.out.println("\nTesting MyMinHeap:");
+        MyMinHeap heap = new MyMinHeap();
+        heap.add(5);
+        heap.add(3);
+        heap.add(8);
+        System.out.println(heap.removeMin()); // 3
+        System.out.println(heap.peek()); // 5
+
+        System.out.println("\nTesting MyArrayList:");
+        MyArrayList<String> arrayList = new MyArrayList<>();
+        arrayList.add("one");
+        arrayList.add("two");
+        arrayList.addFirst("zero");
+        for (String s : arrayList) {
+            System.out.println(s);
         }
-        System.out.println(findMin(arr)); // возвращает минимум
+
+        System.out.println("\nTesting MyLinkedList:");
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.addFirst(0);
+        linkedList.removeLast();
+        for (int val : linkedList) {
+            System.out.println(val);
+        }
     }
 }
-
-// task  2
